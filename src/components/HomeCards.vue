2713cb633bf830473188458a471190b9f2b05ed3
Thyme2be <trinchuaynugoon@gmail.com>
2025-02-18 22:51:25 +0700

    Make dynamic color div on Card, Add simulate database, Display job list and make it dynamic

--- src/components/HomeCards.vue
+++ src/components/HomeCards.vue
@@ -17,7 +17,7 @@ import Card from "./Card.vue";
             Browse Jobs
           </a>
         </Card>
-        <Card>
+        <Card bg="bg-green-100">
           <h2 class="text-2xl font-bold">For Employers</h2>
           <p class="mt-2 mb-4">
             List your job to find the perfect developer for the role
