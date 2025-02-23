6eb7eb5fdeb77e4d2065933eeab59d01a8329612
Thyme2be <trinchuaynugoon@gmail.com>
2025-02-23 14:43:34 +0700

    setting up vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from '@/router/router.js'
 import './assets/main.css'
 import 'primeicons/primeicons.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.mount('#app')
