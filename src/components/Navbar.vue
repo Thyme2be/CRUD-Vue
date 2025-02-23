c19ed527ee70885ffe7452e1550a4c961bc898bd
Thyme2be <trinchuaynugoon@gmail.com>
2025-02-23 17:10:47 +0700

    ีuse RouterLink and add Not Found page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,11 @@
 <script setup>
 import logo from "@/assets/images/logo.png";
+import { useRoute } from "vue-router";
+
+const isActiveLink = (routePath) => {
+  const route = useRoute();
+  return route.path === routePath;
+};
 </script>
 
 <template>
@@ -20,17 +26,30 @@ import logo from "@/assets/images/logo.png";
             <div class="flex space-x-2">
               <RouterLink
                 to="/"
-                class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/') ? ' bg-green-900 ' : ' hover:bg-gray-900 ',
+                  ' text-white rounded-md px-3 py-2 ',
+                ]"
                 >Home</RouterLink
               >
               <RouterLink
                 to="/jobs"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/jobs')
+                    ? ' bg-green-900 '
+                    : ' hover:bg-gray-900 ',
+                  ' text-white rounded-md px-3 py-2 ',
+                ]"
                 >Jobs</RouterLink
               >
               <RouterLink
                 to="/jobs/add"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/jobs/add')
+                    ? ' bg-green-900 '
+                    : ' hover:bg-gray-900 ',
+                  ' text-white rounded-md px-3 py-2 ',
+                ]"
                 >Add Job</RouterLink
               >
             </div>
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
   { path: "/jobs", name: "jobs", component: JobsView },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
 ];
 
 const router = createRouter({
