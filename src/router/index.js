66fdc43d808335d8dc59e158df82ae62ed6e4030
Thyme2be <trinchuaynugoon@gmail.com>
2025-02-24 23:54:57 +0700

    install json-server and make new jobs.json

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,12 @@ import { createWebHistory, createRouter } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
+import JobView from "@/views/JobView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
   { path: "/jobs", name: "jobs", component: JobsView },
+  { path: "/job/:id", name: "job", component: JobView },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
 ];
 
