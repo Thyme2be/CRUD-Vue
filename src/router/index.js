3eb926731312c4dc8b24cd380e0a022f4b3f8359
Thyme2be <trinchuaynugoon@gmail.com>
2025-02-27 22:34:45 +0700

    Make functionality edit job

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,14 @@ import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
 import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
   { path: "/jobs", name: "jobs", component: JobsView },
   { path: "/jobs/:id", name: "job", component: JobView },
   { path: "/jobs/add", name: "add-job", component: AddJobView },
+  { path: "/jobs/edit/:id", name: "edit-job", component: EditJobView },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
 ];
 
