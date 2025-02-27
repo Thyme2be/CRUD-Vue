c385c30ebeada3f9df80617ec58c99d223eb45af
Thyme2be <trinchuaynugoon@gmail.com>
2025-02-27 21:32:24 +0700

    Make AddJobView and edit router jobs

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
   { path: "/jobs", name: "jobs", component: JobsView },
-  { path: "/job/:id", name: "job", component: JobView },
+  { path: "/jobs/:id", name: "job", component: JobView },
+  { path: "/jobs/add", name: "add-job", component: AddJobView },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
 ];
 
